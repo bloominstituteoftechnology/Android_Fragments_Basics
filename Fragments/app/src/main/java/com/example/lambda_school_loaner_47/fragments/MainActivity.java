cd3a5b8700d46ae1bb4852340be6d4fc7a97f7e0
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-14 14:56:12 -0600

    added test data to main

--- Fragments/app/src/main/java/com/example/lambda_school_loaner_47/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/example/lambda_school_loaner_47/fragments/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.lambda_school_loaner_47.fragments;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Person> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = new ArrayList<>();
+
+        list.add(new Person("John", "Brown", "txy.com","8709876543"));
+        list.add(new Person("Jerry", "Chris", "txz.com","8709870000"));
+        list.add(new Person("Gary", "Blue", "txc.com","8709870000"));
+        list.add(new Person("Cherry", "Orange", "txb.com","8709870000"));
+        list.add(new Person("Dale", "Jones", "txc.com","8709870000"));
+        list.add(new Person("Chuck", "Berry", "txd.com","8709870000"));
+        list.add(new Person("Kobe", "Jordan", "txf.com","8709870000"));
+        list.add(new Person("Rick", "Wrestler", "txj.com","8709870000"));
     }
 }
