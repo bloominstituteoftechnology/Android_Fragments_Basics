d56d2270afc483bf99e661ec8f2fc9aebbd67c9f
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-14 14:47:08 -0600

    created a person class

---
+++ Fragments/app/src/main/java/com/example/lambda_school_loaner_47/fragments/Person.java
@@ -0,0 +1,46 @@
+package com.example.lambda_school_loaner_47.fragments;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String firstName, lastName, email, phone;
+
+    public Person(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
