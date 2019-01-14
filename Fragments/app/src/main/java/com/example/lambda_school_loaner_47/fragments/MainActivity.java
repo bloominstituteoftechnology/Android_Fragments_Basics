a3c36866d15f5f255d51e0c52b2b95a78f7ed817
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-14 14:35:15 -0600

    initial

---
+++ Fragments/app/src/main/java/com/example/lambda_school_loaner_47/fragments/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lambda_school_loaner_47.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
