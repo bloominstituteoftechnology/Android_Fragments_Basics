9263841127cfb7f32f2cebbfd94955dfa97ab2ca
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-14 15:43:54 -0800

    Added two pane support for tablets.

--- app/src/main/java/com/joshuahalvorson/android_fragments_basics/MainActivity.java
+++ app/src/main/java/com/joshuahalvorson/android_fragments_basics/MainActivity.java
@@ -1,5 +1,7 @@
 package com.joshuahalvorson.android_fragments_basics;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity implements MtgCardFragment.O
 
     @Override
     public void onListFragmentInteraction(Card item) {
-
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("card", item);
+        if(getResources().getBoolean(R.bool.is_tablet)){
+            final DetailsFragment detailFragment = DetailsFragment.newInstance(item);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_detail, detailFragment).commit();
+        }else{
+            Intent intent = new Intent(getApplicationContext(), PhoneDetailActivity.class);
+            intent.putExtra("card", item);
+            startActivity(intent);
+        }
     }
 }
