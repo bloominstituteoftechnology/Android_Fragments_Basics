7eb2a11f8cfb4fa42d82c60b6f579198a9d98031
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-08 15:27:44 -0400

    Added Recycler view dependency and base list adapter

---
+++ Fragments/app/src/main/java/com/rybarstudios/fragments/LayoutAdapter.java
@@ -0,0 +1,33 @@
+package com.rybarstudios.fragments;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LayoutAdapter extends RecyclerView.Adapter<LayoutAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
