5a047a8f5356b0696d89693bf255f61b0510cb49
VivekV95 <vivekv210@gmail.com>
2019-04-08 14:40:02 -0500

    Added DAO method to get ArrayList of all pokemon

--- app/src/main/java/com/vivekvishwanath/android_fragments_basics/PokemonDao.java
+++ app/src/main/java/com/vivekvishwanath/android_fragments_basics/PokemonDao.java
@@ -2,12 +2,32 @@ package com.vivekvishwanath.android_fragments_basics;
 
 import android.graphics.Bitmap;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class PokemonDao {
 
     private static final String BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+    public static ArrayList<Pokemon> getAllPokemon() {
+        ArrayList<Pokemon> pokemonList = new ArrayList<>();
+        String result = NetworkAdapter.httpGETRequest(BASE_URL);
+        try {
+            JSONObject allPokemonJson = new JSONObject(result);
+            JSONArray pokemonJsonArray = allPokemonJson.getJSONArray("results");
+            for (int i = 0; i < pokemonJsonArray.length(); i++) {
+                Pokemon pokemon = new Pokemon(getPokemon(pokemonJsonArray.getJSONObject(i).getString("name")));
+                pokemonList.add(pokemon);
+            }
+            return pokemonList;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return pokemonList;
+    }
 
     public static JSONObject getPokemon(String nameOrId) {
         JSONObject pokemonJSON = null;
