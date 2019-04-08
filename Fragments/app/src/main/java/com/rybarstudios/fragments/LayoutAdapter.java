48421850d92b9d0760e2153d8d9e8c796c17ab82
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-08 15:48:58 -0400

    Added arrest data model

--- Fragments/app/src/main/java/com/rybarstudios/fragments/LayoutAdapter.java
+++ Fragments/app/src/main/java/com/rybarstudios/fragments/LayoutAdapter.java
@@ -1,33 +1,47 @@
 package com.rybarstudios.fragments;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class LayoutAdapter extends RecyclerView.Adapter<LayoutAdapter.ViewHolder> {
+import java.util.ArrayList;
 
+public class LayoutAdapter extends RecyclerView.Adapter<LayoutAdapter.ArrestViewHolder> {
+
+   Context context;
+   ArrayList<ArrestData> data;
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ArrestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.nfl_arrest_item_list, parent, false);
+        return new ArrestViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ArrestViewHolder arrestViewHolder, int i) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.data.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ArrestViewHolder extends RecyclerView.ViewHolder {
+        View parentLayout;
+        TextView arrestView;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ArrestViewHolder(@NonNull View itemView) {
             super(itemView);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+            arrestView = itemView.findViewById(R.id.arrest_text_view);
         }
     }
+
 }
