e6492bcc8e2b35636c0618b99cf362b648388ac5
heiligbasil <basilief@yahoo.com>
2019-04-08 21:09:07 -0700

    Added a second file with attr.xml bool values for bigger screens

--- app/src/main/java/com/lambdaschool/android_fragments_basics/ItemFragment.java
+++ app/src/main/java/com/lambdaschool/android_fragments_basics/ItemFragment.java
@@ -2,6 +2,8 @@ package com.lambdaschool.android_fragments_basics;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -48,6 +50,11 @@ public class ItemFragment extends Fragment {
         if (getArguments() != null) {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
     }
 
     @Override
--- app/src/main/java/com/lambdaschool/android_fragments_basics/MainActivity.java
+++ app/src/main/java/com/lambdaschool/android_fragments_basics/MainActivity.java
@@ -1,8 +1,13 @@
 package com.lambdaschool.android_fragments_basics;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,10 +19,33 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new ItemFragment()).commit();
     }
 
     @Override
     public void onItemListFragmentInteraction(Item item) {
-        Log.i(TAG,"Fragment interacted");
+        Log.i(TAG, "List Fragment Interacted");
+
+        DetailsFragment detailsFragment = new DetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("item", item);
+        detailsFragment.setArguments(bundle);
+
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,detailsFragment).addToBackStack(null).commit();
+        if (getResources().getBoolean(R.bool.is_tablet)) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_details, detailsFragment)
+                    .addToBackStack(null)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, detailsFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+
     }
 }
